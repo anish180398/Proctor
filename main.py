54c7d64bc2fc4c850c0bd3d06e6ba50eed516942
anish18398 <anish.s18398@pm.me>
2022-03-22 15:59:28 +0530

    Inital Commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+import dlib
+from math import hypot
+from mouth_tracking import *
+from facial_landmarks_detection import *
+from blink_detection import *
+from gaze_detection import *
+#from object_detection import *
+print("Started")
+cap = cv2.VideoCapture(0)
+
+if(cap.isOpened()==False):
+    cap.open()
+
+while True:
+
+	ret , frame = cap.read()
+	
+	faceCount, faces = detectFace(frame)
+	mouthTrack(faces, frame)
+	blinkStatus = isBlinking(faces, frame)
+	eyeStatus = gazeDetection(faces, frame)
+	#objectStatus = detectObject(frame)
+	#print(blinkStatus[2]+' - '+eyeStatus)
+	#print(objectStatus)
+	print(eyeStatus)
+
+
+
+	cv2.imshow('frame',frame)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+cap.release()
+cv2.destroyAllWindows()
